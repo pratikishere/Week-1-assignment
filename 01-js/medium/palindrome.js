9b3cbe7228c543f8e0e799906dd646d1b0be0edd
Pratik <pbpatel1990@yahoo.com>
2023-06-12 22:14:25 +0530

    Add logic for isPalindrome, Add script to run it

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,24 @@
 */
 
 function isPalindrome(str) {
+
+  const len = str.length;
+
+  if(len == 0) return false;
+
+  for(let i = 0; i < len / 2; i++) {
+    if(str[i].toLowerCase() !== str[len - 1 - i].toLowerCase()) {
+      return false;
+    }
+  }
+
   return true;
 }
+
+const output1 = isPalindrome("NanAn");
+console.log(`output1 ${output1}`);
+
+const output2 = isPalindrome("");
+console.log(`output2 ${output2}`);
 
 module.exports = isPalindrome;
