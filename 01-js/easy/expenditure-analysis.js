a390abc2a579e78e5eb6498ffb0d27b7f0bba1e5
Pratik <pbpatel1990@yahoo.com>
2023-06-12 20:12:01 +0530

    Add logic for expenditure-analysis, Add script to run it

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,68 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let output = [];
+  
+  let categories = transactions.reduce( (result, transaction) => {
+   const { category, price } = transaction;
+
+    if(!result[category]) {
+    result[category] = { 'category': category, 'totalSpent': 0 };
+    output.push(result[category]);
+   }
+   result[category].totalSpent += price;
+
+   return result;
+  }, {});
+
+  return output;
 }
+
+const transactions = [{
+  id: 1,
+  timestamp: 1656076800000,
+  price: 40,
+  category: 'Food',
+  itemName: 'Pasta',
+},
+{
+  id: 2,
+  timestamp: 1656259600000,
+  price: 40,
+  category: 'Food',
+  itemName: 'Burger',
+},
+{
+  id: 3,
+  timestamp: 1656019200000,
+  price: 10,
+  category: 'Clothing',
+  itemName: 'T-Shirt',
+},
+{
+  id: 4,
+  timestamp: 1656364800000,
+  price: 300,
+  category: 'Electronics',
+  itemName: 'Laptop',
+},
+{
+  id: 5,
+  timestamp: 1656105600000,
+  price: 250,
+  category: 'Clothing',
+  itemName: 'Jeans',
+},
+{
+  id: 6,
+  timestamp: 1656105600100,
+  price: 555,
+  category: 'Electronics',
+  itemName: 'Mobile',
+},
+];
+
+const output = calculateTotalSpentByCategory(transactions);
+console.log(output);
 
 module.exports = calculateTotalSpentByCategory;
